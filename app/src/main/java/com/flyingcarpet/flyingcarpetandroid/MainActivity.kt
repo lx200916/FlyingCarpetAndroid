39b93f545e05bf8d2dc6992117165e0bc3633ade
Lee <44310445+lx200916@users.noreply.github.com>
2022-11-01 00:25:00 +0800

    Add Animation

--- app/src/main/java/com/flyingcarpet/flyingcarpetandroid/MainActivity.kt
+++ app/src/main/java/com/flyingcarpet/flyingcarpetandroid/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.flyingcarpet.flyingcarpetandroid
 
-import android.R.attr.maxLines
-import android.R.attr.tint
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.OpenableColumns
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.magnifier
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -23,9 +29,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -39,8 +45,11 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.airbnb.lottie.compose.*
 import com.flyingcarpet.flyingcarpetandroid.ui.theme.FlyingCarpetAndroidTheme
 import com.google.accompanist.flowlayout.FlowRow
+import java.text.DecimalFormat
+
 
 const val ICON_SIZE = 28
 
@@ -55,15 +64,38 @@ fun navigateTo(navController: NavController, route: String) {
 
         launchSingleTop = true
         restoreState = true
-    }}
+    }
+}
+
+fun getReadableFileSize(size: Long): String {
+    val BYTES_IN_KILOBYTES = 1024
+    val dec = DecimalFormat("###.#")
+    val KILOBYTES = " KB"
+    val MEGABYTES = " MB"
+    val GIGABYTES = " GB"
+    var fileSize = 0f
+    var suffix = KILOBYTES
+    if (size > BYTES_IN_KILOBYTES) {
+        fileSize = (size / BYTES_IN_KILOBYTES).toFloat()
+        if (fileSize > BYTES_IN_KILOBYTES) {
+            fileSize = fileSize / BYTES_IN_KILOBYTES
+            if (fileSize > BYTES_IN_KILOBYTES) {
+                fileSize = fileSize / BYTES_IN_KILOBYTES
+                suffix = GIGABYTES
+            } else {
+                suffix = MEGABYTES
+            }
+        }
+    }
+    return (dec.format(fileSize) + suffix).toString()
+}
+
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContent {
-
-
             var selectedItem by remember { mutableStateOf(0) }
             val navController = rememberNavController()
             FlyingCarpetAndroidTheme {
@@ -141,30 +173,85 @@ class MainActivity : ComponentActivity() {
 
 }
 
+@SuppressLint("Range")
+fun getFileName(context: Context, uri: Uri, Col: String): String {
+    var result: String? = null
+    if (uri.scheme == "content") {
+        val cursor: Cursor? = context.contentResolver.query(uri, null, null, null, null)
+        try {
+            if (cursor != null && cursor.moveToFirst()) {
+                if (Col == OpenableColumns.SIZE) {
+                    result = getReadableFileSize(cursor.getLong(cursor.getColumnIndex(Col)))
+                } else
+                    result = cursor.getString(cursor.getColumnIndex(Col))
+            }
+        } finally {
+            cursor?.close()
+        }
+    }
+    if (result == null) {
+        result = ""
+    }
+    return result
+}
+
+@SuppressLint("Range")
+fun getFileSize(context: Context, uri: Uri): String {
+    var result: String? = null
+    if (uri.scheme == "content") {
+        val cursor: Cursor? = context.contentResolver.query(uri, null, null, null, null)
+        try {
+            if (cursor != null && cursor.moveToFirst()) {
+                result = cursor.getString(cursor.getColumnIndex(OpenableColumns.SIZE))
+            }
+        } finally {
+            cursor?.close()
+        }
+    }
+    if (result == null) {
+        result = uri.path
+        val cut = result!!.lastIndexOf('/')
+        if (cut != -1) {
+            result = result.substring(cut + 1)
+        }
+    }
+    return result
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Send(navController: NavController?) {
+    var pickedDocUri by remember { mutableStateOf<Uri?>(null) }
+    val launcher =
+        rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            println("selected file URI ${it.data?.data}")
+            pickedDocUri = it.data?.data
+        }
+
 //ExtendedFloatingActionButton(text = {Text()}, onClick = { /*TODO*/ })
     Scaffold(floatingActionButton = {
-        ExtendedFloatingActionButton(text = {
-            Text(
-                "Start",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.ExtraBold
-            )
-        }, icon = {
-            Icon(
-                painterResource(id = R.drawable.outbox_24px),
-                modifier = Modifier,
-                contentDescription = ""
-            )
-        }, onClick = {
-            navigateTo(navController!!, "sending")
-        })
+
+        if (pickedDocUri != null)
+            ExtendedFloatingActionButton(text = {
+                Text(
+                    "Start",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            }, icon = {
+                Icon(
+                    painterResource(id = R.drawable.outbox_24px),
+                    modifier = Modifier,
+                    contentDescription = ""
+                )
+            }, onClick = {
+                navigateTo(navController!!, "sending")
+            })
     }, floatingActionButtonPosition = FabPosition.End) { contentPadding ->
         Column(modifier = Modifier.padding(horizontal = 20.dp)) {
             Spacer(modifier = Modifier.padding(vertical = 10.dp))
             Text(
+
                 "Send Files",
                 fontSize = MaterialTheme.typography.headlineLarge.fontSize,
                 fontWeight = MaterialTheme.typography.headlineLarge.fontWeight,
@@ -178,18 +265,34 @@ fun Send(navController: NavController?) {
                     .clip(RoundedCornerShape(5.dp))
                     .background(MaterialTheme.colorScheme.primaryContainer)
                     .padding(15.dp)
+                    .clickable {
+                        val intent = Intent(
+                            Intent.ACTION_OPEN_DOCUMENT,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                        )
+                            .apply {
+                                addCategory(Intent.CATEGORY_OPENABLE)
+                            }
+                        launcher.launch(intent)
+
+                    }
 
             ) {
+
                 Icon(
-                    painter = painterResource(id = R.drawable.document_24_filled),
+                    painter = painterResource(id = if (pickedDocUri == null) R.drawable.apps_add_in_24_filled else R.drawable.document_24_filled),
                     contentDescription = "Document Icon",
                     modifier = Modifier.size(42.dp),
-                    tint = MaterialTheme.colorScheme.secondary
+                    tint = MaterialTheme.colorScheme.onPrimaryContainer
                 )
                 Spacer(modifier = Modifier.padding(horizontal = 5.dp))
                 Column(modifier = Modifier.weight(1f)) {
                     Text(
-                        "File Name",
+                        if (pickedDocUri == null) "Select Document" else getFileName(
+                            LocalContext.current,
+                            pickedDocUri!!,
+                            OpenableColumns.DISPLAY_NAME
+                        ) ?: "",
                         fontSize = 18.sp,
                         fontWeight = FontWeight.ExtraBold,
                         modifier = Modifier.alpha(0.8f),
@@ -197,7 +300,12 @@ fun Send(navController: NavController?) {
                         overflow = TextOverflow.Ellipsis
                     )
                     Text(
-                        "File Size",
+                        if (pickedDocUri == null) "Click Here to Select Document to Send" else getFileName(
+                            LocalContext.current,
+                            pickedDocUri!!,
+                            OpenableColumns.SIZE
+                        ),
+
                         fontSize = 14.sp,
                         fontWeight = FontWeight.Normal,
                         modifier = Modifier.alpha(0.5f),
@@ -222,7 +330,6 @@ fun Send(navController: NavController?) {
                 fontSize = 18.sp,
             )
             OSPicker()
-
 
 
         }
@@ -285,7 +392,9 @@ data class processItem(var name: String, val description: String)
 @Composable
 fun processScreen() {
     val currentStep by remember { mutableStateOf(0) }
-
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.sending))
+    val progress by animateLottieCompositionAsState(composition, clipSpec = LottieClipSpec.Frame(10,182),  iterations = LottieConstants.IterateForever,
+    )
     val processItems by remember {
         mutableStateOf(
             listOf<processItem>(
@@ -296,34 +405,39 @@ fun processScreen() {
             )
         )
     }
-    Box(modifier = Modifier.height(IntrinsicSize.Max)) {
-        Box(
-            modifier = Modifier
-//            .height(10.dp)
-                .padding(start = ICON_SIZE.dp / 2 + 1.dp)
-                .padding(top = 20.dp, bottom = 20.dp)
+    Column(Modifier.padding(10.dp)) {
 
-        ) {
+        LottieAnimation(composition = composition, progress = progress, contentScale = ContentScale.Inside, modifier = Modifier.weight(1f))
+        Box(modifier = Modifier.height(IntrinsicSize.Max)) {
             Box(
                 modifier = Modifier
-                    .fillMaxHeight()
-                    .width(1.dp)
-                    .background(MaterialTheme.colorScheme.onBackground)
-                    .alpha(0.6f)
-            )
-        }
-        Column(modifier = Modifier, verticalArrangement = Arrangement.spacedBy(10.dp)) {
+//            .height(10.dp)
+                    .padding(start = ICON_SIZE.dp / 2 + 1.dp)
+                    .padding(top = 20.dp, bottom = 20.dp)
 
-            processItems.forEachIndexed { index, processItem ->
-                TimelineDot(
-                    state = currentStep - index,
-                    name = processItem.name,
-                    description = processItem.description
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .width(1.dp)
+                        .background(MaterialTheme.colorScheme.onBackground)
+                        .alpha(0.6f)
                 )
             }
+            Column(modifier = Modifier, verticalArrangement = Arrangement.spacedBy(10.dp)) {
+
+                processItems.forEachIndexed { index, processItem ->
+                    TimelineDot(
+                        state = currentStep - index,
+                        name = processItem.name,
+                        description = processItem.description
+                    )
+                }
+            }
+
+
         }
-
-
+        Spacer(modifier = Modifier.weight(0.3f))
     }
 //    Column(modifier=Modifier.fillMaxWidth()) {
 //        Row(Modifier.fillMaxWidth()) {
